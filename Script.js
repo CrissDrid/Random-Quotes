3a6620269f011c425b665a1633f020384f92f8fd
Cristopher David Diaz Moreno <cdiaz@cuentadealtocosto.org>
2025-07-24 07:21:30 -0600

    Implementa la funcionalidad de citas aleatorias y agrega un sistema de notificación para compartir citas

--- Script.js
+++ Script.js
@@ -1,11 +1,71 @@
+let data = [];
+let currentQuote;
+
 fetch(
-  "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/3-javascrit/challenges/group_1/data/random-quotes.json"
+  "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/3-javascript/challenges/group_1/data/random-quotes.json"
 )
 
   .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+  .then((json) => {
+    data = json;
+    randomQuote()
   })
   .catch((error) => {
-    console.error(response);
+    console.error("Error fetching data:", error);
   });
+
+  function randomQuote() {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomQuote = data[randomIndex]
+
+    currentQuote = randomQuote;
+
+    const authorElement = document.querySelector(".quote-author")
+    authorElement.innerText = randomQuote.author;
+
+    const tagsContainer = document.querySelector(".quote-tags")
+    tagsContainer.innerHTML = "";
+
+    randomQuote.tags.forEach(tags => {
+      //Crea un elemento span en el DOM
+      const tagsElements = document.createElement("span")
+      //Añade un elemento span por cada tag que haya con el estilo del css
+      tagsElements.classList.add("quote-tag");
+      //El contenido de text se cambia por el que se devuelve del json recorriendo el array
+      tagsElements.textContent = tags;
+      //Se inserta el span como hijo en el tagsContainer
+      tagsContainer.appendChild(tagsElements)
+    });
+
+    const quoteElement = document.querySelector(".quote-text")
+    quoteElement.innerText = `“${randomQuote.quote}”`;
+  }
+
+  document.querySelector(".random-quote").addEventListener("click", randomQuote);
+
+  function showToaste(message) {
+    const toast = document.getElementById("toast")
+    toast.textContent = message;
+    toast.classList.add("show")
+
+    clearTimeout(toast.timeout);
+
+    setTimeout(() => {
+      toast.classList.remove("show")
+    }, 2500)
+  }
+
+  function shareQuote() {
+    const shareQuote = `“${currentQuote.quote}” - ${currentQuote.author}`
+
+    navigator.clipboard.writeText(shareQuote)
+      .then(() => {
+        showToaste("Cita copiada en el portapapeles")
+      })
+      .catch((error) => {
+        console.error("Error al copiar al portapapeles: ",error)
+        showToaste("Error al copiar la cita")
+      })
+  }
+
+  document.querySelector(".share-quote").addEventListener("click", shareQuote)
